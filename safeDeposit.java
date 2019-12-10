04b6187dfe52883240869a34d7c0abce9b657d94
redtrev03 <58435274+redtrev03@users.noreply.github.com>
2019-12-10 10:55:17 -0500

    Add files via upload

--- safeDeposit.java
+++ safeDeposit.java
@@ -1,53 +1,73 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.util.List;
 public class safeDeposit {
 
-public static void main(String[] args) {
-//while loop
-    String yes;
-   System.out.println("Welcome to the Safety Deposit Box)");
-   System.out.println("Press 1 to begin");
+	public static void main(String[] args) 
+	{
+		
 
-   Scanner start = new Scanner(System.in);
+	
+	{
+	   System.out.println("Welcome to the Safety Deposit Box)");
+	   System.out.println("Press 1 to begin");
 
-   int resetValue = start.nextInt();
+	   Scanner start = new Scanner(System.in);
 
-List<String> answers = new ArrayList<String>();
+	   int resetValue = start.nextInt();
 
-Scanner items = new Scanner(System.in);
+	List<String> answers = new ArrayList<String>();
 
-while (resetValue == 1){
- 
- System.out.println("How many items would you like to add?");
- 
- int okay = items.nextInt();
+	
 
-for(int i=0; i < okay; i++){
-  String temp = new String();
-  Scanner thinginput = new Scanner(System.in);
-  System.out.println("Name the item:");
-  temp = thinginput.next();
-  
-  
-  answers.add(temp);
-  
-  System.out.println("Your Deposit Box: ");
-  System.out.println(answers);
+	while (resetValue == 1){
+	 String box = new String();
+	 System.out.println("Press D to make a Deposit, Press W to make a Withdrawl?");
+	 Scanner asdf = new Scanner(System.in);
+	 box = asdf.next();
+	 
+if(box.equalsIgnoreCase("d")){	
+	Scanner items = new Scanner(System.in);
+	System.out.println("How many items do you want to deposit");
+	 int amount = items.nextInt();
+
+	for(int i=0; i < amount; i++){
+	  String temp = new String();
+	  Scanner thinginput = new Scanner(System.in);
+	  System.out.println("Name the item:");
+	  temp = thinginput.next();
+	  
+	  
+	  answers.add(temp);
+	  
+	  System.out.println("Your Deposit Box: ");
+	  System.out.println(answers);
+	}
 }
-
-if (resetValue == 1){
-            System.out.println("Type 0 to do another transaction or 2 to stop");
-            resetValue =  start.nextInt();
-        }//reset values for whileloop to make a start and stop solution
-        if (resetValue == 0){
-            resetValue++;
-        }
-        if (resetValue == 2){
-        System.out.println("Have a great day!");
-            break;
-        }
-        }
+else if(box.equalsIgnoreCase("w")) {
+	String maybe = new String();
+	Scanner p = new Scanner(System.in);	
+	System.out.println("Name the item, you would like to withdrawl:");
+	System.out.println(answers);
+	maybe = p.next();
+	
+	answers.remove(answers.indexOf(maybe));
+	System.out.println(answers);
 }
+	if (resetValue == 1){
+	            System.out.println("Type 0 to do another transaction or 2 to stop");
+	            resetValue =  start.nextInt();
+	        }//reset values for whileloop to make a start and stop solution
+	        if (resetValue == 0){
+	            resetValue++;
+	        }
+	        if (resetValue == 2){
+	        System.out.println("Have a great day!");
+	            break;
+	        }
+	        }
+	}
 
+}
 }
