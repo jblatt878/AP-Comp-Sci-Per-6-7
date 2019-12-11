770c48825032b5889286ef8bbaa87c99d8fad539
jblatt878 <58434980+jblatt878@users.noreply.github.com>
2019-12-11 11:17:55 -0500

    Add files via upload

--- UpdatedBankAccount.java
+++ UpdatedBankAccount.java
@@ -1,172 +1,335 @@
 import java.util.Scanner;
+
 public class BankAccount
+
 {
+
+
 
 public static void main(String[] args)
+
 {
+
       BankAccount boadi = new BankAccount();
+
       boadi.BankAccount1();
+
 }
+
+
 
     public BankAccount()
+
 {
-        double checkingBalance;
-        double savingsBalance;
-        String myName; 
-        String myAddress;
-        String myBirthday;
-        int cardNumber;
-        int pin;
-        String accountName;
-        String myPassword;
+
+        checkingBalance = this.getcheckingBalance;
+
+        savingsBalance = this.getsavingsBalance;
+
+        myName = vito1.getmyName; 
+
+        myAddress = vito1.getmyAddress;
+
+        myBirthday = vito1.getmyBirthday;
+
+        cardNumber = vito1.getcardNumber;
+
+        pin = vito1.getpin;
+
+        accountName = vito1.getaccountName;
+
+        myPassword = vito1.getmyPassword;
+
 }
+
     public void BankAccount1()
+
 {
-      BankAccount boadi = new BankAccount();
+
       String end = "g";
+
       while (end.equals("g"))
+
       {
+
         Scanner account = new Scanner(System.in);
+
         System.out.println("Do you wish to access your checking or savings account? c = checking s = savings");
+
         String input = account.next();
+
         if (input.equals("c"))
+
         {
-          boadi.changeChecking();
+
+          this.changeChecking();
+
         }
+
         if (input.equals("s"))
+
         {
-          boadi.changeSavings();
+
+          this.changeSavings();
+
         }
+
         Scanner end1 = new Scanner(System.in);
+
         System.out.println();
+
         System.out.println("Are you done, or do you wish to go back to your account? enter 'g' to go back");
+
         end = end1.next();
+
       }
+
 }
+
+
 
     public BankAccount(double balance)
+
 {
+
         checkingBalance = balance;
+
 }
-    
+
+
+
     public double getcheckingBalance()
+
 {
+
         return checkingBalance;
+
 }
-    
+
+
+
     public void checkingdeposit(double amount)
+
 {
+
         checkingBalance = checkingBalance + amount;
+
 }
+
+
 
     public void checkingdeposit()
+
 {
-      BankAccount boadi = new BankAccount();
+
+
+
       Scanner account2 = new Scanner(System.in); 
+
       System.out.println("How much do you wish to deposit?");
+
       double input2 = account2.nextDouble();
-      boadi.checkingdeposit(input2);
+
+      this.checkingdeposit(input2);
+
       System.out.println();
+
       System.out.println("Your checking balance is:");
-      System.out.println(boadi.getcheckingBalance());
+
+      System.out.println(this.getcheckingBalance());
+
 }
-    
+
+
+
     public void checkingwithdraw(double amount)
+
 {
+
         checkingBalance = checkingBalance - amount;
+
 }
+
+
 
     public void checkingwithdraw()
+
 {
-      BankAccount boadi = new BankAccount();
+
+
       Scanner account2 = new Scanner(System.in); 
+
       System.out.println("How much do you wish to withdraw?");
+
       int input2 = account2.nextInt();
-      boadi.checkingwithdraw(input2);
+
+      this.checkingwithdraw(input2);
+
       System.out.println();
+
       System.out.println("Your checking balance is:");
-      System.out.println(boadi.getcheckingBalance());
+
+      System.out.println(this.getcheckingBalance());
+
 }
+
+
 
     public void changeChecking ()
+
 {
-      BankAccount boadi = new BankAccount();
+
+
       Scanner account1 = new Scanner(System.in); 
+
       System.out.println("Do you wish to deposit or withdraw from checking? w = withdraw d = deposit");
+
       String input1 = account1.next();
+
       if (input1.equals("w"))
+
       {
-        boadi.checkingwithdraw();
+
+        this.checkingwithdraw();
+
       }
+
       if (input1.equals("d"))
+
       {
-        boadi.checkingdeposit();
+
+        this.checkingdeposit();
+
       }
-}
-public double getsavingsBalance()
-{
-       return savingsBalance;
-}
-  
-   public void savingsdeposit1(double amount)
-{
-       savingsBalance = savingsBalance + amount;
-}
- 
-   public void savingsdeposit()
-{
-     BankAccount boadi = new BankAccount();
-     Scanner account2 = new Scanner(System.in);
-     System.out.println("How much do you wish to deposit?");
-     double input2 = account2.nextDouble();
-     boadi.savingsdeposit1(input2);
-     System.out.println();
-     System.out.println("Your savings balance is:");
-     System.out.println(boadi.getsavingsBalance());
-}
-  
-   public void savingswithdraw(double amount)
-{
-       savingsBalance = savingsBalance - amount;
-}
- 
-   public void savingswithdraw()
-{
-     BankAccount boadi = new BankAccount();
-     Scanner account2 = new Scanner(System.in);
-     System.out.println("How much do you wish to withdraw?");
-     int input2 = account2.nextInt();
-     boadi.savingswithdraw(input2);
-     System.out.println();
-     System.out.println("Your savings balance is:");
-     System.out.println(boadi.getsavingsBalance());
-}
- 
-   public void changeSavings ()
-{
-     BankAccount boadi = new BankAccount();
-     Scanner account1 = new Scanner(System.in);
-     System.out.println("Do you wish to deposit or withdraw from savings? w = withdraw d = deposit");
-     String input1 = account1.next();
-     if (input1.equals("w"))
-     {
-       boadi.savingswithdraw();
-     }
-     if (input1.equals("d"))
-     {
-       boadi.savingsdeposit();
-     }
 
 }
-    
-    
+
+public double getsavingsBalance()
+
+{
+
+       return savingsBalance;
+
+}
+
+
+
+   public void savingsdeposit1(double amount)
+
+{
+
+       savingsBalance = savingsBalance + amount;
+
+}
+
+
+
+   public void savingsdeposit()
+
+{
+
+
+     Scanner account2 = new Scanner(System.in);
+
+     System.out.println("How much do you wish to deposit?");
+
+     double input2 = account2.nextDouble();
+
+     this.savingsdeposit1(input2);
+
+     System.out.println();
+
+     System.out.println("Your savings balance is:");
+
+     System.out.println(this.getsavingsBalance());
+
+}
+
+
+
+   public void savingswithdraw(double amount)
+
+{
+
+       savingsBalance = savingsBalance - amount;
+
+}
+
+
+
+   public void savingswithdraw()
+
+{
+
+
+     Scanner account2 = new Scanner(System.in);
+
+     System.out.println("How much do you wish to withdraw?");
+
+     int input2 = account2.nextInt();
+
+     this.savingswithdraw(input2);
+
+     System.out.println();
+
+     System.out.println("Your savings balance is:");
+
+     System.out.println(this.getsavingsBalance());
+
+}
+
+
+
+   public void changeSavings ()
+
+{
+
+
+     Scanner account1 = new Scanner(System.in);
+
+     System.out.println("Do you wish to deposit or withdraw from savings? w = withdraw d = deposit");
+
+     String input1 = account1.next();
+
+     if (input1.equals("w"))
+
+     {
+
+       this.savingswithdraw();
+
+     }
+
+     if (input1.equals("d"))
+
+     {
+
+       this.savingsdeposit();
+
+     }
+
+
+
+}
+
+
+
+
+
   double checkingBalance;
+
   double savingsBalance;
+
   String myName; 
+
   String myAddress;
+
   String myBirthday;
+
   int cardNumber;
+
   int pin;
+
   String accountName;
+
   String myPassword;
 }
