09fef4e0e98c9c2e7291bf60fbea14c4013b3724
deep2048 <58480386+deep2048@users.noreply.github.com>
2019-12-17 10:55:19 -0500

    Update Investment.java

--- Worst-Bank-Ever/Classes/Investment.java
+++ Worst-Bank-Ever/Classes/Investment.java
@@ -32,107 +32,134 @@ class Investments
       System.out.println("You have $" + this.getCD()+ " In your Certificate of Deposit account");
       System.out.println("You have $" + this.getMMA()+ " In your Money Market account");
    
-     //Deposits
-     Scanner keyboard1 = new Scanner(System.in);
-     System.out.print("Enter how much you wish to deposit in your Certificate of Deposit account, there is a 2% interest rate: ");
-      double x = keyboard1.nextDouble();
+   
+   
+   
+   
+   
+   
+   
+   
+    //for while statement
+     int loopy = 0
+   
+   while(loopy!=2)
+   {
+   
+        //Deposits
+         Scanner keyboard1 = new Scanner(System.in);
+        System.out.print("Enter how much you wish to deposit in your Certificate of Deposit account, there is a 2% interest rate: ");
+        double x = keyboard1.nextDouble();
     
   
-     System.out.print("Enter how much you wish to deposit in your Money Market account, there is a 5% interest rate: ");
-      double y = keyboard1.nextDouble();
+         System.out.print("Enter how much you wish to deposit in your Money Market account, there is a 5% interest rate: ");
+         double y = keyboard1.nextDouble();
     
     
     
     
-      //Withdrawls
-       System.out.print("Enter how much you wish to withdraw in your Certificate of Deposit account, there will be a twenty dollar penalty: ");
-      double a = keyboard1.nextDouble();
+         //Withdrawls
+         System.out.print("Enter how much you wish to withdraw in your Certificate of Deposit account, there will be a twenty dollar penalty: ");
+         double a = keyboard1.nextDouble();
     
-      //CD withdraw fee
-      if (a!=0)
-          {
-              a=a+20;
-          }
+          //CD withdraw fee
+         if (a!=0)
+              {
+                 a=a+20;
+              }
     
      
-       System.out.print("Enter how much you wish to withdraw from your Money Market account, if your account balance falls below $500 there will be a twenty dollar penalty: ");
-      double b = keyboard1.nextDouble();
+         System.out.print("Enter how much you wish to withdraw from your Money Market account, if your account balance falls below $500 there will be a twenty dollar penalty: ");
+        double b = keyboard1.nextDouble();
     
     
-      //make transfer variable to prevent error
-      double trans4 = 0;
-      double trans6 = 0;
+        //make transfer variable to prevent error
+        double trans4 = 0;
+        double trans6 = 0;
    
     
-      //Tranfer
-      System.out.println("Would you like to transfer funds from your checking account? Type 1 for yes or 2 for no.");
-      int trans = keyboard1.nextInt();
-      if (trans == 1)
-      {
-          System.out.println("Would you like to transfer to your CD account? Type 1 for yes or 2 for no.");
-          int trans3 = keyboard1.nextInt();
-          if (trans3 == 1)
-          {
-              System.out.println("How much?");
-              trans4 = keyboard1.nextDouble();
-          }
-          else if (trans3 == 2)
-          {
-               System.out.println();
-          }
+         //Tranfer
+        System.out.println("Would you like to transfer funds from your checking account? Type 1 for yes or 2 for no.");
+        int trans = keyboard1.nextInt();
+        if (trans == 1)
+         {
+           System.out.println("Would you like to transfer to your CD account? Type 1 for yes or 2 for no.");
+           int trans3 = keyboard1.nextInt();
+           if (trans3 == 1)
+           {
+               System.out.println("How much?");
+                  trans4 = keyboard1.nextDouble();
+              }
+            else if (trans3 == 2)
+             {
+                 System.out.println();
+             }
         
-          System.out.println("Would you like to transfer to your MMA account? Type 1 for yes or 2 for no.");
-          int trans5 = keyboard1.nextInt();
-          if (trans5 == 1)
-          {
-              System.out.println("How much?");
-              trans6 = keyboard1.nextDouble();
-          }
-          else if (trans5 == 2)
-          {
-               System.out.println();
-          }
+             System.out.println("Would you like to transfer to your MMA account? Type 1 for yes or 2 for no.");
+            int trans5 = keyboard1.nextInt();
+            if (trans5 == 1)
+            {
+                System.out.println("How much?");
+                trans6 = keyboard1.nextDouble();
+            }
+             else if (trans5 == 2)
+             {
+                  System.out.println();
+             }
         
         
-      }
+        }
     
-      if (trans == 2)
-      {
-         System.out.println();
-      }
+        if (trans == 2)
+         {
+            System.out.println();
+         }
    
     
     
    
-      //Deposit,withdraw,transfer
-      this.setCD(0 + x -  a + trans4);
-      this.setMMA(0 + y - b + trans6);
+         //Deposit,withdraw,transfer
+        this.setCD(0 + x -  a + trans4);
+        this.setMMA(0 + y - b + trans6);
     
     
-      //Take away from Checking account
-      vito.setcheckingBalance(vito.getcheckingBalance() - trans4 - trans6);
+         //Take away from Checking account
+        vito.setcheckingBalance(vito.getcheckingBalance() - trans4 - trans6);
       
     
     
-      //Overdraft fees
-      this.MMAoverdraw();
+        //Overdraft fees
+        this.MMAoverdraw();
    
-      //Interest
-      this.CDinterest();
-      this.MMAinterest();
+        //Interest
+        this.CDinterest();
+        this.MMAinterest();
     
-      //Round
-      this.CDround();
-      this.MMAround();
+        //Round
+        this.CDround();
+        this.MMAround();
    
     
     
    
-      System.out.println();
-      System.out.println("You now have $"+ vito.getcheckingBalance() +"in your Checking Account ");
-      System.out.println("You now have $" + this.getCD() + " in your Certificate of Deposit account");
-      System.out.println("You now have $" + this.getMMA() + " in your Money Market account");
+             System.out.println();
+             System.out.println("You now have $"+ vito.getcheckingBalance() +"in your Checking Account ");
+             System.out.println("You now have $" + this.getCD() + " in your Certificate of Deposit account");
+             System.out.println("You now have $" + this.getMMA() + " in your Money Market account");
+             
+             
+        System.out.print("Would you like to continue using our bank's investment services, type 1 for yes, 2 for no");
+        loopy = keyboard1.nextInt();
+             
+             
+             
+             
+             
   }
+  
+  
+  
+  
    //Accessor methods
   public double getCD()
   {
