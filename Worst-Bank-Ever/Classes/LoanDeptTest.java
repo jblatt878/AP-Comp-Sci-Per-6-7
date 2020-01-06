7925f7006f157fc74cd18909bbe0953977085134
alc1429 <58434972+alc1429@users.noreply.github.com>
2020-01-06 10:49:48 -0500

    Add files via upload

--- Worst-Bank-Ever/Classes/LoanDeptTest.java
+++ Worst-Bank-Ever/Classes/LoanDeptTest.java
@@ -7,60 +7,60 @@ public static void main(String[] args)
     double car; 
     double college;
     int score;
-    double salary;
+    double productprice;
     LoanDept moneyOwed = new LoanDept();
     String name;
-    System.out.print ( "Enter type of loan (lowercase): 'mortgage' 'car' 'college' 'home equity'");
+    System.out.print ( "Enter type of loan (lowercase): 'mortgage' 'car' 'college' 'home equity'\n");
     Scanner kind = new Scanner (System.in);
     name = kind.nextLine();
-    System.out.print ( "Enter your salary: $");
-    Scanner income = new Scanner (System.in);
-    salary = income.nextDouble();
+    System.out.print ( "Enter the price of the product you'd like a loan for: $");
+    Scanner price = new Scanner (System.in);
+    productprice = price.nextDouble();
 //MORTGAGE
  if(name.equals("mortgage"))
      {
          double mname1 = 0;
-System.out.print ("You can loan up to $" + (salary * 2) + ". Enter how much you want to loan: $");
+System.out.print ("We can loan you up to $" + (productprice * 0.8) + ". Enter how much you want to be loaned: $");
     Scanner loan = new Scanner (System.in);
     mname1 = loan.nextDouble();
         moneyOwed.moveTo(0.0369);
         moneyOwed.mloan(mname1);
-        System.out.print("Money Owed per Month over 30 years: $" + moneyOwed.getMortgage());
+        System.out.print("Money Owed per Month over 30 years: $" + moneyOwed.getMortgage() + ". Your interest rate is 3.69%.");
      }
 
 //CAR
       else if(name.equals("car"))
      {
          double carname = 0;
-System.out.print ("You can loan up to $" + (salary * 1.1) + ". Enter how much you want to loan: $");
+System.out.print ("We can loan you up to $" + (productprice * 0.8) + ". Enter how much you want to be loaned: $");
     Scanner loan1 = new Scanner (System.in);
     carname = loan1.nextDouble();
         moneyOwed.moveTo(0.0421);
         moneyOwed.cloan(carname);
-        System.out.print("Money Owed per Month over 5 years: $" + moneyOwed.getCar());
+        System.out.print("Money Owed per Month over 5 years: $" + moneyOwed.getCar() + ". Your interest rate is 4.21%.");
      }
      
 //COLLEGE (https://www.nerdwallet.com/blog/loans/student-loans/much-borrow-college/)
         else if(name.equals("college"))
      {
         double cname = 0;
-System.out.print ("You can loan up to $" + (salary * 1.5) + ". Enter how much you want to loan: $");
+System.out.print ("We can loan you up to $" + (productprice * 0.8) + ". Enter how much you want to be loaned: $");
     Scanner loan2 = new Scanner (System.in);
     cname = loan2.nextDouble();
         moneyOwed.moveTo(0.0453);
         moneyOwed.collegeloan(cname);
-        System.out.print("Money Owed per Month over 10 years: $" + moneyOwed.getCollege());
+        System.out.print("Money Owed per Month over 10 years: $" + moneyOwed.getCollege() + ". Your interest rate is 4.53%.");
      }
 //HOME EQUITY
       else if(name.equals("home equity"))
       {
           double hname = 0;
-System.out.print ("You can loan up to $" + (salary * 2) + ". Enter how much you want to loan: $");
+System.out.print ("We can loan you up to $" + (productprice * 0.8) + ". Enter how much you want to be loaned: $");
     Scanner loan3 = new Scanner (System.in);
     hname = loan3.nextDouble();
          moneyOwed.moveTo(0.0565);
          moneyOwed.hloan(hname);
-         System.out.print("Money Owed per Month over 15 years: $" + moneyOwed.getHome()); 
+         System.out.print("Money Owed per Month over 15 years: $" + moneyOwed.getHome() + ". Your interest rate is 5.65%."); 
          //FIX NUMBERS TO ROUND 2 DECIMAL PLACES// 
       }
 }
@@ -98,7 +98,7 @@ class LoanDept
     }
     public void mloan(double amount)
     {
-    myMortgage = myMortgage + amount * (interest/12);
+    myMortgage = Math.round(((amount * ((Math.pow((1 + interest / 12), 30*12))/360)) * 100.0) / 100.0); 
     }
     public double getCar()
     {
@@ -112,7 +112,7 @@ class LoanDept
     
     public void cloan(double amount)
     {
-        myCar = myCar + amount * (interest/12);
+        myCar = Math.round(((amount * ((Math.pow((1 + interest / 12), 5*12))/60)) * 100.0) / 100.0);  
     }
     public double getCollege()
     {
@@ -127,7 +127,7 @@ class LoanDept
     public void collegeloan(double amount)
     {
     
-        myCollege = myCollege + amount * (interest/12);
+        myCollege = Math.round(((amount * ((Math.pow((1 + interest / 12), 10*12))/120)) * 100.0) / 100.0);  
     }
     public double getHome()
     {
@@ -141,7 +141,7 @@ class LoanDept
     
     public void hloan(double amount)
     {
-        myHome = myHome + amount * (interest/12);
+        myHome = Math.round((( amount * ((Math.pow((1 + interest / 12), 15*12))/180)) * 100.0) / 100.0); 
     }
      double interest;
      double moneyOwed;
